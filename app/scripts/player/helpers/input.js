5e0c6b106b9e4e700fcff910a880ee2019be490d
dylanmensaert <dylan.mensaert@gmail.com>
2013-07-10 02:46:56 +0200

    Deleted  unnecessary "input" template and view in "player\helpers"

--- app/scripts/player/helpers/input.js
+++
@@ -1,9 +0,0 @@
-define([
-    "Ember", "text!player/helpers/input.handlebars"
-], function (Ember, template) {
-    "use strict";
-
-    return Ember.View.extend({
-        defaultTemplate : Ember.Handlebars.compile(template)
-    });
-});
