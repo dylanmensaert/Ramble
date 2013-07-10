09d1309d810bcc73b0f6657ab25499d1bf69c4e7
dylanmensaert <dylan.mensaert@gmail.com>
2013-07-10 22:20:22 +0200

    Added accordion to player list.

--- app/scripts/player/helpers/item.js
+++ app/scripts/player/helpers/item.js
@@ -4,6 +4,13 @@ define([
     "use strict";
 
     return Ember.View.extend({
-        defaultTemplate : Ember.Handlebars.compile(template)
+        defaultTemplate : Ember.Handlebars.compile(template),
+        classNames : ["accordion-group"],
+        targetItemId : Ember.computed(function () {
+            return "#" + this.get("itemId");
+        }).property("itemId"),
+        itemId : Ember.computed(function () {
+            return this.get("itemIdName") + this.get("content.id");
+        }).property("itemIdName", "content.id")
     });
 });
--- app/scripts/player/list/view.js
+++ app/scripts/player/list/view.js
@@ -5,8 +5,12 @@ define([
 
     return Ember.View.extend({
         defaultTemplate : Ember.Handlebars.compile(template),
+        accordionId : "accordion-player-list",
         playerItem : playerItem.extend({
-            tagName : "li"
+            itemIdName : "player-list",
+            accordionId : Ember.computed(function () {
+                return "#" + this.get("parentView.accordionId");
+            }).property("parentView.accordionId")
         })
     });
 });
