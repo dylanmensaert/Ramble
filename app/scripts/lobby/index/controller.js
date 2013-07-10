2ed3a699aac07e5fbf6f95fbde4ab298c74f6221
dylanmensaert <dylan.mensaert@gmail.com>
2013-07-10 04:32:22 +0200

    Fixed syntax bug because of rearranging code.

--- app/scripts/lobby/index/controller.js
+++ app/scripts/lobby/index/controller.js
@@ -7,6 +7,7 @@ define([
         documentTitle : Ember.computed(function () {
             return this._super() + this.get("title");
         }).property("title", "controllers.lobby.documentTitle"),
+        needs : ["application"],
         //TODO: Change to server side functionality
         leave : function () {
             this.get("players").removeObject(this.get("controllers.application.account"));
@@ -17,6 +18,14 @@ define([
         kick : function (player) {
             this.get("players").removeObject(player);
             this.get("transaction").commit();
-        }
+        },
+        //TODO: Weird bug when binding account from application or login controller
+        //accountBinding : "controllers.application.account",
+        //isOwner : Ember.computed(function () {
+        //    return this.get("account.id") === this.get("owner.id");
+        //}).property("account.id", "owner.id"),
+        isOwner : Ember.computed(function () {
+            return this.get("controllers.application.account") === this.get("owner");
+        }).property("controllers.application.account", "owner")
     });
 });
--- app/scripts/lobby/root/controller.js
+++ app/scripts/lobby/root/controller.js
@@ -5,15 +5,6 @@ define([
 
     return Ember.ObjectController.extend({
         documentTitle : "Lobby",
-        isLeaf : false,
-        needs : ["application"],
-        //TODO: Weird bug when binding account from application or login controller
-        //accountBinding : "controllers.application.account",
-        //isOwner : Ember.computed(function () {
-        //    return this.get("account.id") === this.get("owner.id");
-        //}).property("account.id", "owner.id"),
-        isOwner : Ember.computed(function () {
-            return this.get("controllers.application.account") === this.get("owner");
-        }).property("controllers.application.account", "owner")
+        isLeaf : false
     });
 });
