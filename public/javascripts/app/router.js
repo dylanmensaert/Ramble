1c2fad818a8709fd38efb054ad631d775dd42704
dimitrisuls <dimitri.suls@gmail.com>
2013-07-29 17:55:00 +0200

    Improved ".jshintrc"-files. Simplified "router.map()".

--- public/javascripts/app/router.js
+++ public/javascripts/app/router.js
@@ -27,58 +27,39 @@ define([
             App.Router.map(function () {
                 this.resource("login");
 
-                this.resource("lobby",
-                    {
-                        path : "lobby"
-                    }, function () {
-                        this.route("index",
-                            {
-                                path : ":lobby_id"
-                            });
-                        this.route("create",
-                            {
-                                path : "create"
-                            });
-                        this.route("edit",
-                            {
-                                path : ":lobby_id/edit"
-                            });
-                        this.route("delete",
-                            {
-                                path : ":lobby_id/delete"
-                            });
-                        this.route("list",
-                            {
-                                path : "list"
-                            });
-                    });
+                this.resource("lobby", function () {
+                    this.route("index",
+                        {
+                            path : ":lobby_id"
+                        });
+                    this.route("edit",
+                        {
+                            path : ":lobby_id/edit"
+                        });
+                    this.route("delete",
+                        {
+                            path : ":lobby_id/delete"
+                        });
+                    this.route("list");
+                    this.route("create");
+                });
 
-                this.resource("player",
-                    {
-                        path : "player"
-                    }, function () {
-                        this.route("index",
-                            {
-                                path : ":player_id"
-                            });
-                        this.route("create",
-                            {
-                                path : "create"
-                            });
-                        this.route("edit",
-                            {
-                                path : ":player_id/edit"
-                            });
-                        this.route("delete",
-                            {
-                                path : ":player_id/delete"
-                            });
-                        this.route("list",
-                            {
-                                path : "list"
-                            });
-                    });
-
+                this.resource("player", function () {
+                    this.route("index",
+                        {
+                            path : ":player_id"
+                        });
+                    this.route("edit",
+                        {
+                            path : ":player_id/edit"
+                        });
+                    this.route("delete",
+                        {
+                            path : ":player_id/delete"
+                        });
+                    this.route("list");
+                    this.route("create");
+                });
             });
         }
     };
