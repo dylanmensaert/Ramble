e7783ffa12d329b0a3a24b159a899a8392fb353d
dimitrisuls <dimitri.suls@gmail.com>
2013-07-30 04:05:38 +0200

    Minor code improvements.

--- public/javascripts/lobby/init.js
+++ public/javascripts/lobby/init.js
@@ -8,42 +8,6 @@ define(function (require) {
         initialize : function () {
             App.Lobby = require("lobby/model");
 
-            //TODO: Create API in Express.js
-            App.Lobby.FIXTURES = [
-                {
-                    "id" : 1,
-                    "title" : "Game Dylan en Dimitri",
-                    "password" : "zonneschijn",
-                    "maxMembers" : "5",
-                    "owner" : [1],
-                    "members" : [2, 3, 5, 8]
-                },
-                {
-                    "id" : 2,
-                    "title" : "customgame",
-                    "password" : "Peter",
-                    "maxMembers" : "7",
-                    "owner" : [8],
-                    "members" : [4, 9, 3]
-                },
-                {
-                    "id" : 3,
-                    "title" : "winners lobby!",
-                    "password" : "Dolfijn",
-                    "maxMembers" : "12",
-                    "owner" : [6],
-                    "members" : [5, 3, 8, 1]
-                },
-                {
-                    "id" : 4,
-                    "title" : "eten",
-                    "password" : "open",
-                    "maxMembers" : "7",
-                    "owner" : [8],
-                    "members" : []
-                }
-            ];
-
             App.LobbyRoute = require("lobby/root/route");
             App.LobbyController = require("lobby/root/controller");
             App.LobbyView = require("lobby/root/view");
@@ -51,10 +15,6 @@ define(function (require) {
             App.LobbyIndexRoute = require("lobby/index/route");
             App.LobbyIndexController = require("lobby/index/controller");
             App.LobbyIndexView = require("lobby/index/view");
-
-            App.LobbyCreateRoute = require("lobby/create/route");
-            App.LobbyCreateController = require("lobby/create/controller");
-            App.LobbyCreateView = require("lobby/create/view");
 
             App.LobbyEditRoute = require("lobby/edit/route");
             App.LobbyEditController = require("lobby/edit/controller");
@@ -67,6 +27,10 @@ define(function (require) {
             App.LobbyListRoute = require("lobby/list/route");
             App.LobbyListController = require("lobby/list/controller");
             App.LobbyListView = require("lobby/list/view");
+
+            App.LobbyCreateRoute = require("lobby/create/route");
+            App.LobbyCreateController = require("lobby/create/controller");
+            App.LobbyCreateView = require("lobby/create/view");
         }
     };
 });
--- public/javascripts/player/init.js
+++ public/javascripts/player/init.js
@@ -8,72 +8,6 @@ define(function (require) {
         initialize : function () {
             App.Player = require("player/model");
 
-            App.Player.FIXTURES = [
-                {
-                    "id" : 1,
-                    "username" : "donut",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [1],
-                    "joinedLobbies" : [3]
-                },
-                {
-                    "id" : 2,
-                    "username" : "pizaa",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [],
-                    "joinedLobbies" : [1]
-                },
-                {
-                    "id" : 3,
-                    "username" : "zizima",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [],
-                    "joinedLobbies" : [1, 2, 3]
-                },
-                {
-                    "id" : 4,
-                    "username" : "madeentje",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [],
-                    "joinedLobbies" : [2]
-                },
-                {
-                    "id" : 5,
-                    "username" : "cryptex",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [],
-                    "joinedLobbies" : [3, 1]
-                },
-                {
-                    "id" : 6,
-                    "username" : "heca",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [3],
-                    "joinedLobbies" : []
-                },
-                {
-                    "id" : 7,
-                    "username" : "bosbes",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [],
-                    "joinedLobbies" : []
-                },
-                {
-                    "id" : 8,
-                    "username" : "graaaah",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [2, 4],
-                    "joinedLobbies" : [3, 1]
-                },
-                {
-                    "id" : 9,
-                    "username" : "antiliaan",
-                    "email" : "aaaa@gmail.com",
-                    "ownedLobbies" : [],
-                    "joinedLobbies" : [2]
-                }
-            ];
-
             App.PlayerRoute = require("player/root/route");
             App.PlayerController = require("player/root/controller");
             App.PlayerView = require("player/root/view");
@@ -81,10 +15,6 @@ define(function (require) {
             App.PlayerIndexRoute = require("player/index/route");
             App.PlayerIndexController = require("player/index/controller");
             App.PlayerIndexView = require("player/index/view");
-
-            App.PlayerCreateRoute = require("player/create/route");
-            App.PlayerCreateController = require("player/create/controller");
-            App.PlayerCreateView = require("player/create/view");
 
             App.PlayerEditRoute = require("player/edit/route");
             App.PlayerEditController = require("player/edit/controller");
@@ -97,6 +27,10 @@ define(function (require) {
             App.PlayerListRoute = require("player/list/route");
             App.PlayerListController = require("player/list/controller");
             App.PlayerListView = require("player/list/view");
+
+            App.PlayerCreateRoute = require("player/create/route");
+            App.PlayerCreateController = require("player/create/controller");
+            App.PlayerCreateView = require("player/create/view");
         }
     };
 });
