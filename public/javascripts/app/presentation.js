2aa0f70d5d055a3987a171d5db4873965b595849
dimitrisuls <dimitri.suls@gmail.com>
2013-08-26 03:05:16 +0200

    Updated the config of Ember-EasyForm to use the right CSS-classes to properly display the form and its validation-errors. Reopened the Ember.TextField-class to include the "form-control" CSS-class by default, to automatically make use of Bootstrap's styling. Fixed the positioning of the menu's items. Simplified the login-form by making use of Ember-EasyForm.

--- public/javascripts/app/presentation.js
+++ public/javascripts/app/presentation.js
@@ -1,20 +1,25 @@
 define(function (require) {
     "use strict";
 
-    var EmberEasyForm = require("EmberEasyForm");
+    var Ember = require("Ember"),
+        EmberEasyForm = require("EmberEasyForm");
 
     //TODO: Require only the needed Bootstrap-plugins?
     require("Bootstrap");
 
     return {
         initialize : function () {
+            Ember.TextField.reopen({
+                classNames : ["form-control"]
+            });
+
             EmberEasyForm.Config.registerWrapper("default", {
                 formClass : "",
-                fieldErrorClass : "text-danger",
-                errorClass : "help-inline",
-                hintClass : "help-block",
-                labelClass : "",
+                fieldErrorClass : "has-error",
                 inputClass : "form-group",
+                errorClass : "help-block",
+                hintClass : "help-block",
+                labelClass : "control-label",
                 wrapControls : false,
                 controlsWrapperClass : ""
             });
