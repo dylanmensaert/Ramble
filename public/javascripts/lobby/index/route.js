29f1095b084e6f4dcebef23484a7964ebbc0470a
dylanmensaert <dylan.mensaert@gmail.com>
2013-08-29 00:31:02 +0200

    Fixed transition bug.

--- public/javascripts/lobby/index/route.js
+++ public/javascripts/lobby/index/route.js
@@ -5,11 +5,15 @@ define(function (require) {
 
     return Ember.Route.extend({
         //TODO: Transition is undefined (providedModelsArray: ) if transitionTo() didn't happen.
+        hasSavedTransition: false,
         afterModel: function (model, transition) {
-            if (!this.controllerFor("application").get("isLoggedIn") && !this.controllerFor("login").get("lastTransition")) {
+            if (!this.controllerFor("application").get("isLoggedIn") && !this.get("hasSavedTransition")) {
                 this.controllerFor("login").set("lastTransition", transition);
+                this.set("hasSavedTransition", true);
 
                 this.transitionTo("lobby");
+            } else {
+                this.set("hasSavedTransition", false);
             }
         }
     });
