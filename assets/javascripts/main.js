481605f1431ce7af4bb1d6faa47836b79565149b
dimitrisuls <dimitri.suls@gmail.com>
2013-10-20 23:50:11 +0200

    Fix Travis.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -10,7 +10,8 @@
             'ember-data': '../bower_components/ember-data-shim/ember-data',
             //TODO: Use Dockyards' easyform/validations via their bower-registry.
             'ember-easyform': '../bower_components/ember-dockyard-validations/easyForm/ember-easyForm-1.0.0.beta.1',
-            'ember-validations': '../bower_components/ember-dockyard-validations/validations/ember-validations',
+            //TODO: Build version from Dockyards validations
+            'ember-validations': '../bower_components/ember-dockyard-validations/validations/ember-validations-1.0.0.beta.1',
             handlebars: '../bower_components/handlebars.js/dist/handlebars.runtime',
             jquery: '../bower_components/jquery/jquery',
             bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
