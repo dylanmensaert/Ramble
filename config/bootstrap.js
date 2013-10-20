8696cbd6f8271e0e5dedda3ac1b1e0cb88d807d3
dimitrisuls <dimitri.suls@gmail.com>
2013-10-20 02:03:55 +0200

    Fix to make passport.js work with websockets!

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -12,7 +12,32 @@
 
 module.exports.bootstrap = function (cb) {
 
-    // It's very important to trigger this callack method when you are finished
-    // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+    //TODO: Fix to make passport.js work with websockets!
+    //see http://stackoverflow.com/questions/17365444/sails-js-passport-js-authentication-through-websockets/18343226#18343226
+    var passport = require('passport'),
+        initialize = passport.initialize(),
+        session = passport.session(),
+        http = require('http'),
+        methods = ['login', 'logIn', 'logout', 'logOut', 'isAuthenticated', 'isUnauthenticated'];
+
+    sails.removeAllListeners('router:request');
+    sails.on('router:request', function (request, response) {
+        initialize(request, response, function () {
+            session(request, response, function (error) {
+                var index;
+
+                if (error) {
+                    sails.config[500](500, request, response);
+                } else {
+                    for (index = 0; index < methods.length; index += 1) {
+                        request[methods[index]] = http.IncomingMessage.prototype[methods[index]].bind(request);
+                    }
+
+                    sails.router.route(request, response);
+                }
+            });
+        });
+    });
+
     cb();
 };
