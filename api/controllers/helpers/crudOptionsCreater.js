b5c9d00f2e7642e9fe57a79fc2d9a3f513ffb7d6
dylanmensaert <dylan.mensaert@gmail.com>
2013-11-09 14:41:21 +0100

    Decided only to implement completely necessary checks. (subjected to change).

--- api/controllers/helpers/crudOptionsCreater.js
+++ api/controllers/helpers/crudOptionsCreater.js
@@ -1,19 +1,10 @@
 'use strict';
-var getModelType = function (request) {
-        if (request.target) {
-            return sails.models[request.target.controller];
-        }
-    },
-    getValues = function (request) {
-        if (request.params) {
-            return request.params.all();
-        }
-    };
 
 module.exports = function (request, response) {
     return {
-        modelType: getModelType(request),
-        values: getValues(request),
+        //TODO: Check if request.target and request.params isn't empty!
+        modelType: sails.models[request.target.controller],
+        values: request.params.all(),
         error: function (error) {
             response.error(error);
         },
