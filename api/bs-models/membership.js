aa5b0d82aec2eb504ae478b9c95c18640cdb8ed2
dylanmensaert <dylan.mensaert@gmail.com>
2013-12-10 21:34:50 +0100

    Removed membership TODO

--- api/bs-models/membership.js
+++ api/bs-models/membership.js
@@ -3,7 +3,6 @@
 
 var Bookshelf = require('./bookshelf');
 //TODO: player = require('./player');
-//TODO: Owner/OwnedLobbies not returning object after name change + through() implementation.
 
 module.exports = Bookshelf.Model.extend({
     tableName: 'memberships',
