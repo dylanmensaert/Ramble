8a397493c438f71ffcb49b753587de7fd5e7f183
dimitrisuls <dimitri.suls@gmail.com>
2013-12-22 21:07:33 +0100

    Install underscore via npm. Update Jade.

--- api/helpers/toCamelCase.js
+++ api/helpers/toCamelCase.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('underscore');
+
 module.exports = function (attrs) {
     return _.reduce(attrs, function (memo, value, key) {
         memo[_.str.camelize(key)] = value;
