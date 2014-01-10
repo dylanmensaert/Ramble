ec20367c41cdfde2afc905b9b1216d1b21a4891b
dimitrisuls <dimitri.suls@gmail.com>
2014-01-10 22:02:59 +0100

    Update .jscs.json with the new options. Update dependencies.

--- assets/javascripts/init/router.js
+++ assets/javascripts/init/router.js
@@ -17,35 +17,29 @@ define(function(require) {
         this.resource('login');
 
         this.resource('player', function() {
-            this.route('index',
-                {
-                    path: ':player_id'
-                });
-            this.route('edit',
-                {
-                    path: ':player_id/edit'
-                });
-            this.route('delete',
-                {
-                    path: ':player_id/delete'
-                });
+            this.route('index', {
+                path: ':player_id'
+            });
+            this.route('edit', {
+                path: ':player_id/edit'
+            });
+            this.route('delete', {
+                path: ':player_id/delete'
+            });
             this.route('list');
             this.route('create');
         });
 
         this.resource('lobby', function() {
-            this.route('index',
-                {
-                    path: ':lobby_id'
-                });
-            this.route('edit',
-                {
-                    path: ':lobby_id/edit'
-                });
-            this.route('delete',
-                {
-                    path: ':lobby_id/delete'
-                });
+            this.route('index', {
+                path: ':lobby_id'
+            });
+            this.route('edit', {
+                path: ':lobby_id/edit'
+            });
+            this.route('delete', {
+                path: ':lobby_id/delete'
+            });
             this.route('list');
             this.route('create');
         });
