37f08dfb8fc20676c02913cc006959adc0ac6be6
dimitrisuls <dimitri.suls@gmail.com>
2014-02-03 22:22:12 +0100

    Update bower.json.

--- assets/javascripts/init/presentation.js
+++ assets/javascripts/init/presentation.js
@@ -10,6 +10,7 @@ define(function(require) {
         classNames: ['form-control']
     });
 
+    //TODO: Ember automatically binds to 'autofocus' nowadays
     Ember.FocussedTextField = Ember.TextField.extend({
         attributeBindings: ['autofocus'],
         autofocus: 'autofocus'
