889dd335df7ca1e9980eaa081c1bce32a073c858
dylanmensaert <dylan.mensaert@gmail.com>
2014-09-06 23:13:32 +0200

    Removed Todo related to last commit.

--- assets/javascripts/player/index/controller.js
+++ assets/javascripts/player/index/controller.js
@@ -4,7 +4,6 @@ define(function(require) {
     var Ember = require('ember');
 
     return Ember.ObjectController.extend({
-        // TODO: Template - partial to lobby instead of membership
         // TODO: Not equal when they are the same record? Bug in ember-data, or just compare the ID's?
         isOwnerOfAccount: function() {
             return this.get('session.account') === this.get('model');
