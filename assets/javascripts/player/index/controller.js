9aadc89449ee9517f9be9632615532aebc5fa07c
dylanmensaert <dylan.mensaert@gmail.com>
2014-09-06 23:12:49 +0200

    Fixed rendering of partials with other context.

--- assets/javascripts/player/index/controller.js
+++ assets/javascripts/player/index/controller.js
@@ -4,6 +4,7 @@ define(function(require) {
     var Ember = require('ember');
 
     return Ember.ObjectController.extend({
+        // TODO: Template - partial to lobby instead of membership
         // TODO: Not equal when they are the same record? Bug in ember-data, or just compare the ID's?
         isOwnerOfAccount: function() {
             return this.get('session.account') === this.get('model');
